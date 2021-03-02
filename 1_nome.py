03de1341345f753498d2c22a683cf5801a603af3
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-02 11:24:05 -0300

    Add files via upload

---
+++ 1_nome.py
@@ -0,0 +1,4 @@
+#curso em video
+#1. crie um script python que leia o nome de uma pessoa e mostre a mensagem de boas vindas de acordo com o que foi digitado
+nome = input ("Qual é o seu nome? ")
+print ("Olá ", nome, ", seja bem vinda!")
---
+++ 2_nasc.py
@@ -0,0 +1,8 @@
+#curso em video
+#2. crie um script python que leia o dia, o mês e o ano de nascimeno de uma pessoa e mostre uma mensagem com a data formatada
+dia = input ("Qual é o dia do seu nascimento? ")
+mes = input ("Qual é o mês do seu nascimento? ")
+ano = input ("Qual é o ano do seu nascimento? ")
+
+print ("Sua data de nascimento é", dia, "/", mes, "/", ano+".")
+print ("Você nasceu no dia", dia, "de", mes, "de", ano+".")
