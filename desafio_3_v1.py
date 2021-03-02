7337182f1920699fa0f9d4059310dcc51b048f70
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-02 11:27:26 -0300

    Update and rename desafio3_v2.py to desafio_3_v1.py

--- desafio_3_v1.py
+++ desafio_3_v1.py
@@ -2,7 +2,5 @@
 #3. crie um script python que leia dois números e tente mostrar a soma entre eles
 num1 = int (input ("Digite um número: "))
 num2 = int (input ("Digite mais um número: "))
-
 soma = num1 + num2
-
 print ("A soma de", num1,"e", num2, "é", soma, "!")
