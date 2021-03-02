e18e7fc0fa40b314a399b5313149796d9b7f2216
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-02 11:51:07 -0300

    Add files via upload

---
+++ desafio_04.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele
+#aqui iremos trabalhar com métodos de teste de tipo
+v = input('Digite algo:')
+print('Você digitou um número?', v.isnumeric())
+print('São números entre 0 e 9?', v.isdigit())
+print('Uma letra/palavra ou número?', v.isalnum())
+print('Você digitou alguma letra/palavra?', v.isalpha())
+print('O que você digitou tem somente letras maiúsculas?', v.isupper())
+print('O que você digitou tem somente letras minúsculas?', v.islower())
+print('É um título?', v.istitle())
+print('É um espaço???', v.isspace())
