5f1cc15c9f1c1e1cd7e27b81e361038e1ddd4f2a
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-02 11:32:08 -0300

    Add files via upload

---
+++ desafio_03.py
@@ -0,0 +1,6 @@
+# crie um script python que leia dois números e tente mostrar a soma entre eles
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+# print('A soma de', n1,'e', n2,'é', s)
+print('A soma de {} e {} é {}'.format(n1, n2, s))
