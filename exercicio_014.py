7b5598a6dcdb86f5bedbb680b09d7d750fa108e9
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-03 19:58:10 -0300

    Add files via upload

---
+++ exercicio_014.py
@@ -0,0 +1,4 @@
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+cel = float(input('Temperatura em Celsius: '))
+far = 1.8 * cel + 32
+print('{:.2f}ºC equivalem a {:.2f}ºF'.format(cel, far))
