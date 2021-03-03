fa8c29e5bc8e9addcecda05efa623d0e711fa7c1
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-03 16:54:50 -0300

    Update exercicio_03_v2.py

--- exercicio_03_v2.py
+++ exercicio_03_v2.py
@@ -3,4 +3,5 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite mais um número: '))
 s = n1 + n2
 # print('A soma de', n1,'e', n2,'é', s)
+# print('A soma de {} e {} é {}'.format(n1, n2, n1 + n2))
 print('A soma de {} e {} é {}'.format(n1, n2, s))
