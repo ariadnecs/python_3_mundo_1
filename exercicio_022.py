695fe93c40c870ef8748cbd8c2b926eb7fd5562d
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-08 16:59:23 -0300

    Update exercicio_022.py

--- exercicio_022.py
+++ exercicio_022.py
@@ -3,17 +3,18 @@
 # Quantas letras no total sem considerar espaços. Quantas letras tem o primeiro nome.
 nome = input('Digite seu nome completo: ')
 
+print('Analisando seu nome...\n')
+print('Olá {}'.format(nome))
+
 maiusculas = nome.upper()
 minusculas = nome.lower()
+print('Seu nome em maiúsculas é {}.'.format(maiusculas))
+print('Seu nome em minúsculas é {}.'.format(minusculas))
+
 remove = nome.strip() # remove espaços excedentes no início e no final de 'nome'
 remove2 = remove.replace(' ', '') # substitui espaços pela 'falta de espaços', ''; aqui o nome será guardado sem espaços
 contartodo = len(remove2) #conta as letras totais
 nomelista = remove.split() #agora sim é uma lista, o primeiro nome ocupa a posição 'O', o segundo nome a posição '1' ...
 contarprimeiro = len(nomelista[0])# para contar a quantidade de letras do primeiro nome preciso me referir à posição '0'
-
-print('Analisando seu nome...\n')
-print('Olá {}'.format(nome))
-print('Seu nome em maiúsculas é {}.'.format(maiusculas))
-print('Seu nome em minúsculas é {}.'.format(minusculas))
 print('Seu nome completo é composto por {} letras.'.format(contartodo))
 print('Sendo que o primeiro nome tem {} letras!'.format(contarprimeiro))
