905c1473bc5f9f00574c3bd72f15f43e3183cd5e
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-08 18:03:02 -0300

    Delete exercicio_024.py

--- exercicio_024.py
+++
@@ -1,6 +0,0 @@
-# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
-citty = str(input('Digite o nome de uma cidade: '))
-cidade = citty.strip()
-cidade1 = cidade.upper()
-print('Nome da cidade: {}.'.format(cidade1))
-print('A cidade que vc digitou inicia com a palavra *Santo*? {}'.format('SANTO' in cidade1))
