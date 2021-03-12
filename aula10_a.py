680e489d4df9cc0480cff941e6e4ca23c944e30d
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-12 17:36:08 -0300

    Update aula10_a.py

--- aula10_a.py
+++ aula10_a.py
@@ -1,6 +1,6 @@
 nome = str(input('Qual é o seu nome? ')).strip().title()
-if nome == 'Spok':
-    print('Que bom te você aqui!')
+if nome == 'Spock':
+    print('Vida longa e próspera!')
 else:
     print('Olá, {}.'.format(nome))
-print('Vida longa e póspera!')
+    
