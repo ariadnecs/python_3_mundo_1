97666a5857a8d83885442b53f49ebe9d3f1db97f
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-12 17:37:14 -0300

    Update exercicio_028.py

--- exercicio_028.py
+++ exercicio_028.py
@@ -1,4 +1,6 @@
-# Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+# Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 
+# e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
+# O programa deverá escrever na tela se o usuário venceu ou perdeu.
 from random import randint
 num = int(input('Digite um número inteiro entre 0 a 5 e tente adivinhar qual o número computador ecolheu: '))
 numpc = randint(0, 5)
